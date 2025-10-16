9a0c5e8167ffa4f44cf3b76e68786e1f4f8da51d
surajgiri55446 <surajgiri55446@gmail.com>
2025-10-16 12:01:21 +0530

    Your commit message here

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -186,15 +186,7 @@ const Login = () => {
                   </div>
                 </div>
 
-                {/* Forgot Password Link */}
-                <div className="text-right">
-                  <Link
-                    to="/forgot-password"
-                    className="text-sm text-blue-600 hover:text-blue-700 font-medium transition-colors duration-200"
-                  >
-                    Forgot your password?
-                  </Link>
-                </div>
+               
 
                 {/* Error Message */}
                 {error && (
